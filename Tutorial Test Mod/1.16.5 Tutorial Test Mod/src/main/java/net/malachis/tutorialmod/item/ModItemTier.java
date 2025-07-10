65fffcb7e46f5fb4ebf8b771682a583f56699d08
Salternate <superdiamonds123456@gmail.com>
2025-07-10 01:56:14 -0700

    Void Diamond Tools (Added)

---
+++ Tutorial Test Mod/1.16.5 Tutorial Test Mod/src/main/java/net/malachis/tutorialmod/item/ModItemTier.java
@@ -0,0 +1,60 @@
+package net.malachis.tutorialmod.item;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum ModItemTier implements IItemTier {
+
+    VOID_DIAMOND(7, 1777, 8.7f, 3.7f, 17, () -> Ingredient.fromItems(ModItems.VOID_DIAMOND.get())),
+    //SINGULARITY(77, 7777, 77f, 77f, 77, () -> Ingredient.fromItems(ModItems.VOID_DIAMOND.get()))
+    ;
+
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    ModItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+        this.harvestLevel = harvestLevel;
+        this.maxUses = maxUses;
+        this.efficiency = efficiency;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairMaterial = new LazyValue<>(repairMaterial);
+    }
+
+    @Override
+    public int getMaxUses() {
+        return maxUses;
+    }
+
+    @Override
+    public float getEfficiency() {
+        return efficiency;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return attackDamage;
+    }
+
+    @Override
+    public int getHarvestLevel() {
+        return harvestLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return repairMaterial.getValue();
+    }
+}
--- Tutorial Test Mod/1.16.5 Tutorial Test Mod/src/main/java/net/malachis/tutorialmod/item/ModItems.java
+++ Tutorial Test Mod/1.16.5 Tutorial Test Mod/src/main/java/net/malachis/tutorialmod/item/ModItems.java
@@ -2,7 +2,7 @@ package net.malachis.tutorialmod.item;
 
 import net.malachis.tutorialmod.TutorialMod;
 import net.malachis.tutorialmod.item.custom.VoidPolarityDisplacementChannelingDevice;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 //import net.minecraft.item.ItemGroup;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -14,14 +14,32 @@ public class ModItems
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
+
     public static final RegistryObject<Item> VOID_DIAMOND = ITEMS.register("void_diamond",
             () -> new Item(new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
 
     public static final RegistryObject<Item> ENDSTONE_ENCASED_DIAMOND = ITEMS.register("endstone_encased_diamond",
             () -> new Item(new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP).maxStackSize(8)));
 
+
     public static final RegistryObject<Item> VOID_POLARITY_DISPLACEMENT_CHANNELING_DEVICE = ITEMS.register("void_polarity_displacement_channeling_device",
             () -> new VoidPolarityDisplacementChannelingDevice(new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP).maxStackSize(1)));
+
+
+    public static final RegistryObject<Item> VOID_DIAMOND_SWORD = ITEMS.register("void_diamond_sword",
+            () -> new SwordItem(ModItemTier.VOID_DIAMOND, 3, -2.3f, new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> VOID_DIAMOND_PICKAXE = ITEMS.register("void_diamond_pickaxe",
+            () -> new PickaxeItem(ModItemTier.VOID_DIAMOND, 1, -2.7f, new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> VOID_DIAMOND_AXE = ITEMS.register("void_diamond_axe",
+            () -> new AxeItem(ModItemTier.VOID_DIAMOND, 5, -2.9f, new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> VOID_DIAMOND_SHOVEL = ITEMS.register("void_diamond_shovel",
+            () -> new ShovelItem(ModItemTier.VOID_DIAMOND, 1.5f, -2.9f, new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
+
+    public static final RegistryObject<Item> VOID_DIAMOND_HOE = ITEMS.register("void_diamond_hoe",
+            () -> new HoeItem(ModItemTier.VOID_DIAMOND, -3, 0.1f, new Item.Properties().group(ModItemGroup.TUTORIAL_GROUP)));
 
     public static void register(IEventBus eventBus)
     {
